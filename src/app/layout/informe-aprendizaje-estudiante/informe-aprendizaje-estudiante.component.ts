f5313dcacb199837dabac833ef5a40fb72ef8ae8
willian <wils14@hotmail.es>
2019-07-07 00:26:27 -0500

    service component

--- src/app/layout/informe-aprendizaje-estudiante/informe-aprendizaje-estudiante.component.ts
+++ src/app/layout/informe-aprendizaje-estudiante/informe-aprendizaje-estudiante.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/rest.service';
+import { InfAprendizajeEstudiante } from '../modelos/informeAprendizajeEstudiante.model';
 
 @Component({
   selector: 'app-informe-aprendizaje-estudiante',
@@ -7,9 +9,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformeAprendizajeEstudianteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Service: RestService) { }
 
   ngOnInit() {
   }
 
+  public data: any;
+
+  sistema: InfAprendizajeEstudiante = {
+    idInformeAprendizaje: 0,
+    descripcion: "",
+    tipo: "",
+    fecha: "",
+    horaIngreso:"",
+    horaSalida:"",
+    horasTotales:"",
+    prioridad: 0,
+  }
+
+  id: number;
+    idInformeAprendizaje: number;
+    descripcion: string;
+    tipo: string;
+    fecha: Date;
+    horaIngreso: string;
+    horaSalida: string;
+    horasTotales: string;
+    prioridad: number;
+
+
+
+  cargarDatos() {
+    this.Service.getData("segs").subscribe((data: InfAprendizajeEstudiante) => {
+      //data acumula todo dato que el servicio tenga
+      this.data = data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  agregarDatos() {
+    this.Service.addData(this.sistema, "segadds")
+      .subscribe((data: InfAprendizajeEstudiante) => {
+        this.data = data; 
+      },
+        error => {
+          console.log("error al guardar", error);
+        });
+  }
+
+ borrarDatos(id: number){
+    this.Service.deleteData( this.data.id ).subscribe( () => {
+      
+      console.log(this.data.id);
+    });
+    // this.onCloseConfirm();
+  }
+
+actualizarDatos() {
+    this.Service.updateData(this.data, "segupds")
+      .subscribe(() => {
+        
+      }, error => {
+        console.log("Error al actualizar", error);
+      })
+  }
+
+
+   
 }
--- src/app/layout/modelos/informeAprendizajeEstudiante.model.ts
+++ src/app/layout/modelos/informeAprendizajeEstudiante.model.ts
@@ -1,9 +1,9 @@
 export class InfAprendizajeEstudiante {
-    id: number;
+    id?: number;
     idInformeAprendizaje: number;
     descripcion: string;
     tipo: string;
-    fecha: Date;
+    fecha: string;
     horaIngreso: string;
     horaSalida: string;
     horasTotales: string;
